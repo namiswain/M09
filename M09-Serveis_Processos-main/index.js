6d9c4df9937fcab08fffb93a3e1416b682cb7914
namiswain <60893632+namiswain@users.noreply.github.com>
2020-10-21 17:15:45 +0200

    1

--- M09-Serveis_Processos-main/index.js
+++ M09-Serveis_Processos-main/index.js
@@ -6,11 +6,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 let jugador = {
+    posicion:'',
+    alias: '', 
     nombre:'',
     apellido: '',
     score:''
    };
-
+let jugadores = [{
+    posicio: 3,
+    alies: "jperez",
+    nom: "Jose",
+    congnom: "Perez",
+    score: 1000
+},
+{
+    posicio: 1,
+    alies: "jsanz",
+    nom: "Juan",
+    congnom: "Sanz",
+    score: 950
+},
+{
+    posicio: 2,
+    alies: "mgutierrez",
+    nom: "Maria",
+    congnom: "Gutierrez",
+    score: 850
+}
+]
 let respuesta = {
     error: false,
     codigo: 200,
@@ -33,7 +56,6 @@ app.get('/hola', function (req, res) {
 app.listen(3000, () => {
  console.log("El servidor está inicializado en el puerto 3000");
 });
-
 
 app.post('/gamer', function (req, res){
     var nombrej = req.body.nombre || '';
@@ -77,4 +99,20 @@ app.post('/gamer', function (req, res){
         }
     }
     res.send(respuesta);
+});
+
+app.get('/ranking', function (req, res){
+
+
+    jugadores.sort((a,b) => (a.score < b.score ? 1: -1));
+    for (i=0;i<jugadores.length;i++)
+    {
+        jugadores[i].posicio=i+1;
+    }
+    res.send(jugadores);
+
+});
+
+app.get('/jugadores'(alies), function (req, res){
+    res.send(jugadores);
 });
