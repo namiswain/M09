a46db909ed4d624d2073452275b59b095c2892d4
namiswain <60893632+namiswain@users.noreply.github.com>
2020-10-22 22:43:12 +0200

    2.0

--- M09-Serveis_Processos-main/index.js
+++ M09-Serveis_Processos-main/index.js
@@ -12,6 +12,7 @@ let jugador = {
     apellido: '',
     score:''
    };
+
 let jugadores = [{
     posicio: 3,
     alies: "jperez",
@@ -103,7 +104,6 @@ app.post('/gamer', function (req, res){
 
 app.get('/ranking', function (req, res){
 
-
     jugadores.sort((a,b) => (a.score < b.score ? 1: -1));
     for (i=0;i<jugadores.length;i++)
     {
@@ -113,6 +113,137 @@ app.get('/ranking', function (req, res){
 
 });
 
-app.get('/jugadores'(alies), function (req, res){
-    res.send(jugadores);
+app.get('/jugadores/:user', function (req, res){
+    respuesta.error = true;
+        for(i= 0; i < jugadores.length; i++)
+        {
+            if(jugadores[i].alies == req.params.user) 
+            {
+                res.send(jugadores[i]);
+                respuesta.error = false;
+            }
+        }
+        if(respuesta.error)
+        {
+            respuesta = {
+            error: true, 
+            codi: 504,
+            mensaje: "El jugador no existe"
+            }
+            res.send(respuesta);
+        }
+});
+
+
+app.post('/jugadores/:alies', function (req, res){
+    var nomPlayer = req.body.nom || '';
+    var cognomPlayer = req.body.congnom || '';
+    var scorePlayer = req.body.score || '';
+    var aliesPlayer = req.body.alies || '';
+    var posicioPlayer = req.body.posicio || ''; 
+    respuesta.error = false;
+
+    if(nomPlayer == ''|| cognomPlayer == '' || parseInt(scorePlayer) <= 0 || aliesPlayer == ''|| posicioPlayer == '') {
+        respuesta = {
+            error: true,
+            codi: 502,
+            missatge: 'El campo alias, nombre, apellido y score son requeridos'
+        };
+    } 
+    else if(aliesPlayer != req.params.user) {
+        respuesta = {
+            error: true,
+            codi: 503,
+            missatge: "El jugador ya existe"
+        };
+    } 
+    else 
+    {
+        for(var x = 0; x < jugadores.length ; x++)
+        {
+            if(jugadores[x].nom == nomPlayer && jugadores[x].congnom == cognomPlayer && jugadores[x].alies == aliesPlayer) 
+            {
+                respuesta = {
+                    error: true,
+                    codi: 503,
+                    mensaje: "El jugador ya fue creado previamente"
+                };
+            }
+        }
+        if(!respuesta.error)
+        {
+            jugadores.push(
+                {
+                    posicio: posicioPlayer,
+                    alies: aliesPlayer,
+                    nom: nomPlayer,
+                    congnom: cognomPlayer,
+                    score: scorePlayer
+                }
+            ) 
+            respuesta = {                 
+                error: false, 
+                codigo: 200,               
+                mensaje: 'Jugador Creado',   
+                respuesta: jugadores[jugadores.length - 1]          
+            };
+        }
+    }
+});  
+
+app.put('/jugadores/:alies', function (req, res){
+    var nomPlayer = req.body.nom || '';
+    var cognomPlayer = req.body.congnom || '';
+    var scorePlayer = req.body.score || '';
+    var aliesPlayer = req.body.alies || '';
+    var posicioPlayer = req.body.posicio || ''; 
+    respuesta.error = false;
+
+
+    if(nomPlayer == '' || cognomPlayer == '' || parseInt(scorePlayer) <= 0|| aliesPlayer == ''|| posicioPlayer == '') {
+        respuesta = {
+            error: true,
+            codi: 502,
+            mensaje: 'El campo alias, nombre, apellido y score son requeridos'
+        };
+    } 
+    else if(aliesPlayer != req.params.user) {
+        respuesta = {
+            error: true,
+            codigo: 504,
+            mensaje: 'El jugador ya existe'
+        };
+    } 
+    else {
+        for(var x = 0; x < jugadores.length ; x++)
+        {
+            if(jugadores[x].nom == nomPlayer && jugadores[x].congnom == cognomPlayer && jugadores[x].alies == aliesPlayer) 
+            {
+                jugadores[x] = {
+                    posicio: posicioPlayer,
+                    alies: aliesPlayer,
+                    nom: nomPlayer,
+                    congnom: cognomPlayer,
+                    score: scorePlayer
+                };
+
+                respuesta = {
+                    error: false,
+                    codi: 505,
+                    mensaje: 'jugador actualizado',
+                    respuesta: jugadores[x]
+                };
+                playerFound = true;
+            }
+        }
+        if(!buscarjugador)
+        {
+            respuesta = {
+                error: true,
+                codi: 504,
+                mensaje: 'El jugador existe'
+            };
+        }
+    }
+    res.send(respuesta);
 });
